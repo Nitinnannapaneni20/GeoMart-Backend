752acdd9fe09ec1781cb3312de7c7d8b8aa87365
nitin <nitinnannapaneni@gmail.com>
2025-02-11 17:15:56 +0000

    trying to fix api problem with communicating with front-end

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main() {
 
        // Allow frontend to access backend
         router.Use(cors.New(cors.Config{
-            AllowOrigins:     []string{"http://13.40.3.183", "http://localhost:3000"},
+            AllowOrigins:     []string{"*"},
             AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
             AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
             AllowCredentials: true,
