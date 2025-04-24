99b3477878e263c87a4bd278d4e43ebca2ad8ac5
nitin <nitinnannapaneni@gmail.com>
2025-04-24 16:38:25 +0100

    routes file changed

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -12,6 +12,6 @@ func UserRoutes(router *gin.Engine, db *gorm.DB) {
 
     router.GET("/api/user_data", middleware.JWTMiddleware(), controllers.GetUserData(db))
     router.POST("/api/profile/create-if-not-exist", middleware.JWTMiddleware(), controllers.CreateUserIfNotExists(db))
-    router.POST("/api/profile/get", middleware.JWTMiddleware(), controllers.GetUserBySub(db))
+    router.GET("/api/profile/get", middleware.JWTMiddleware(), controllers.GetUserBySub(db))
     router.POST("/api/profile/update", middleware.JWTMiddleware(), controllers.UpdateUserProfile(db))
 }
